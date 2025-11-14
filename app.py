0e6b6a60ad61c1888da5da67dd4158b012419f28
ImManas1 <manaspawar1926@gmail.com>
2025-11-14 22:42:44 +0530

    TitleChanges2

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import pickle
 import numpy as np
-import pandas as pd   # <-- REQUIRED to load df.pkl
+import pandas as pd   
 
-# load data + model
+
 df = pickle.load(open("df.pkl", "rb"))
 pipe = pickle.load(open("pipe.pkl", "rb"))
 
